f50224824a803b4d5cbcca79ca59b3a74360fa07
Rcastillo87 <rubex87@gmail.com>
2025-09-05 21:49:33 -0500

    update de cambios en base de datos

--- sql.sql
+++ sql.sql
@@ -1,20 +1,3 @@
---delete campo de proyecto
-'val_obra_blanca',
-'val_obra_blanca_materiales',
-'val_obra_carpinteria',
-'val_carpinteria_materiales',
-'pres_otros'
-'observacion'
-
--- ademas  se agrega
-'cedula_cliente',
-'tipo_doc_cliente',
-'area_privada' float
-
--- ademas  se agrega
-'fec_inicio',  null
-'fec_fin_estimado', null
-'id_user' null
 
 ALTER TABLE proyectos
     -- Eliminar columnas que ya no se usan
@@ -26,7 +9,6 @@ ALTER TABLE proyectos
     DROP COLUMN val_carpinteria_materiales,
 
     -- Agregar columnas nuevas
-    ADD COLUMN cedula_cliente VARCHAR(20) NULL AFTER nombre_cliente;
     ADD COLUMN cedula_cliente varchar(50) NULL AFTER direccion,
     ADD COLUMN tipo_doc_cliente int(11) NULL AFTER cedula_cliente,
     ADD COLUMN id_user_comercial bigint(20) NULL AFTER id_user_carpinteria,
@@ -39,7 +21,27 @@ ALTER TABLE proyectos
     ADD COLUMN opcion int(11) NULL AFTER entrega_obra_por,
     ADD COLUMN por_inicia int(11) NULL AFTER opcion;
 
+ALTER TABLE proyectos
     MODIFY COLUMN fec_inicio DATE NULL;
-    MODIFY COLUMN id_user INT NULL;
+ALTER TABLE proyectos
+    MODIFY COLUMN id_user BIGINT NULL;
 
 
+CREATE TABLE IF NOT EXISTS `entregables` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nombre_estregable` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla aescala.entregable_proyecto
+CREATE TABLE IF NOT EXISTS `entregable_proyecto` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id_entregable` bigint(20) NOT NULL,
+  `id_proyecto` bigint(20) NOT NULL,
+  `cantidad` bigint(20) NOT NULL,
+  `valor_total` bigint(20) NOT NULL,
+  `tx_entregable` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
