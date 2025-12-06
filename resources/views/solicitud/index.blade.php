0edf25a91e170319667568f293fe1072d28f982a
Rcastillo87 <rubex87@gmail.com>
2025-12-06 15:08:30 -0500

    se oculata boton crear solicitud para analistas

--- resources/views/solicitud/index.blade.php
+++ resources/views/solicitud/index.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 @section('content')
     @include('solicitud.filter')
-    <div class="flex justify-end text-center mb-3">
-        <x-secondary-button class="ms-4" href="{{ route('solicitud.create')}}">
-            Crear Solicitud
-        </x-secondary-button>
-    </div>
+
+    @if (Auth::user()->isAdmin || Auth::user()->isColab)
+        <div class="flex justify-end text-center mb-3">
+            <x-secondary-button class="ms-4" href="{{ route('solicitud.create')}}">
+                Crear Solicitud
+            </x-secondary-button>
+        </div>
+    @endif
+
     <div class="relative overflow-x-auto rounded-lg border border-gray-200">
         <table class="w-full text-left text-sm text-gray-500">
             <x-table-header :headers="$headers" />
