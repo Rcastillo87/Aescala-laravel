bab43aa3c39dd24a5d2d63ec61aff27d9908370c
Rcastillo87 <52466256+Rcastillo87@users.noreply.github.com>
2026-01-20 15:06:44 -0500

    Fix route check for 'solicitud.create' view

--- resources/views/solicitud/create.blade.php
+++ resources/views/solicitud/create.blade.php
@@ -82,7 +82,7 @@
 
             <!-- Botón alineado a la derecha siempre en la parte inferior -->
             <div class="flex justify-end mt-6 space-x-4">
-                @isroute('solicitud.create')
+                @if (request()->routeIs('solicitud.create'))
                     <a  href="#" id="btnLimpiar" class="px-4 py-2 bg-red-500 hover:bg-red-400 rounded-lg flex items-center text-white">
                         <svg class="w-6 h-6 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
