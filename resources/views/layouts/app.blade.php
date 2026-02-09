78f3b466afffcc045062db1bfd1b571051e99550
Rcastillo87 <52466256+Rcastillo87@users.noreply.github.com>
2026-02-09 08:49:33 -0500

    se reversa cambio en app.balde de js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,6 +66,7 @@
         @endif
         <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
         <script src="{{asset('js/chart.min.js')}}"></script>
+        <script src="{{asset('js/app.js')}}"></script>
         @yield('scripts')
     </body>
 </html>
