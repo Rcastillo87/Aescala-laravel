2f71b0d099285f8afbf0f18959ffdff730728c66
Rcastillo87 <52466256+Rcastillo87@users.noreply.github.com>
2026-02-17 15:32:13 -0500

    Update ProyectoController.php

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -57,7 +57,7 @@ class ProyectoController extends Controller
         $hoy = Carbon::today();
         $title = 'Lista de Proyectos';
         $estado = Proyecto::$estado;
-        $items = Proyecto::with(['tareas', 'finanzas'])->when(Request('nombre_proyecto'), function ($query, $nombre_proyecto) {
+        $items = Proyecto::with(['tareas', 'finanzas', 'entreProyecto'])->when(Request('nombre_proyecto'), function ($query, $nombre_proyecto) {
             return $query->whereRaw('LOWER(nombre_proyecto) LIKE LOWER(?)', ["%$nombre_proyecto%"]);
         })
             ->when(Request('nombre_cliente'), function ($query, $nombre_cliente) {
