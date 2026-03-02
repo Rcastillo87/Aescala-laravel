2105543e72cf8d1bb3dc4ce4f158565d9388b72d
Rcastillo87 <52466256+Rcastillo87@users.noreply.github.com>
2026-03-02 16:00:58 -0500

    Update fallback value for fechaTexto
    Change fallback value for fechaTexto to null instead of '--'.

--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -147,7 +147,7 @@ class Proyecto extends Model
 
         $carbon = Carbon::parse($this->fecha_firma);
         $carbon->locale('es');
-        $fechaTexto = $this->fecha_firma? $carbon->translatedFormat('d \d\e F \d\e Y') : '--';
+        $fechaTexto = $this->fecha_firma? $carbon->translatedFormat('d \d\e F \d\e Y') : null;
 
         // Preparar entregables para la vista
         $entregables = $this->entreProyecto->map(function($e){
