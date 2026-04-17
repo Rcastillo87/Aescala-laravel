b3980c0a524b88d98ed0ef28caf0a5c9f479b46a
Rcastillo87 <52466256+Rcastillo87@users.noreply.github.com>
2026-04-17 13:59:57 -0500

    Update filter.blade.php

--- resources/views/solicitud/filter.blade.php
+++ resources/views/solicitud/filter.blade.php
@@ -54,9 +54,8 @@
                             5 => 'Require Aprobacion',
                             6 => 'Aprobado'
                         ];
-                        $estadosItems = unset(self::$estadosItems[1]);
+                        $estadosItems = unset($estadosItems[1]);
                         $arr = array_merge($estadosItems, $arr);
-                        dd($arr);
                     @endphp
                     <div class="p-2 shrink-0 w-[40]">
                         <x-input-label for="id_estado_item" :value="__('Estado Item')" />
